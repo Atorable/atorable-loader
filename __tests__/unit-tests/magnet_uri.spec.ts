c32953ad6cd290c5c51fd76e124dc4e96a5baf9a
serge <serge.thompson@gmail.com>
2022-05-05 10:46:49 -0400

    update tests to convert torrent file name

--- __tests__/unit-tests/magnet_uri.spec.ts
+++ __tests__/unit-tests/magnet_uri.spec.ts
@@ -5,15 +5,18 @@ describe('status integration tests', () => {
     // beforeAll(async () => {})
 
     it('can make a magnetURI', async () => {
+        const relPath = 'upload/test-file-1.txt',
+            regex = /.*(?=\.)/gim,
+            torrentName = regex.exec(relPath)![0] + '.torrent' // TODO find all ! and deal
         const { magnetURI, torrentBuf, infoHash } =
             await GetMagnetAndTorrentBuf(
                 new Buffer('test'),
-                'test-file-1.txt',
-                'test.torrent',
-                'https://www.atorable.com/services/'
+                relPath,
+                torrentName,
+                'https://www.atorable.com/'
             )
         expect(magnetURI).toBe(
-            'magnet:?xt=urn:btih:11bd88f9231cd2365360b4de1df92f9e20d8437d&dn=test-file-1.txt&tr=wss%3A%2F%2Fbitt.atorable.com&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwww.atorable.com%2Fservices%2Ftest-file-1.txt&xs=https%3A%2F%2Fwww.atorable.com%2Fservices%2Ftest.torrent'
+            'magnet:?xt=urn:btih:11bd88f9231cd2365360b4de1df92f9e20d8437d&dn=test-file-1.txt&tr=wss%3A%2F%2Fbitt.atorable.com&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwww.atorable.com%2Fupload%2Ftest-file-1.txt&xs=https%3A%2F%2Fwww.atorable.com%2Fupload%2Ftest-file-1.torrent'
         )
     })
 
