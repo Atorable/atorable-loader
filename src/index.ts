595d1171a6d1e6ff87a5e19482341da1813a5081
serge <serge.thompson@gmail.com>
2022-05-31 21:47:55 -0400

    re-edit message

--- src/index.ts
+++ src/index.ts
@@ -59,8 +59,7 @@ export default async function loader(
 
         if (mURI?.error)
             return callback(
-                mURI.error.message +
-                    `\n${mURI.error.message}\nCannot build magnet URI\n` +
+                `\n${mURI.error}\nCannot build magnet URI\n` +
                     `Update Plan: \x1b[1;34m${updatePlan}\x1b[0m\n`
             ) //blue
 
